4739c9243399660f47eb6ef7d9cc300eb784a836
敏强 卢 <luminqiang@outlook.com>
2019-07-08 18:30:08 +0800

    增加时间和基础验证类

---
+++ BasicValidatorHelper.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace CommonLibrary
+{
+    /// <summary>
+    /// 常用的基础验证
+    /// </summary>
+    public class BasicValidatorHelper
+    {
+        /// <summary>
+        /// 验证传入值是否为数字（数字：000000为合法数字）
+        /// </summary>
+        /// <param name="input">待验证值</param>
+        /// <returns>是否为数字</returns>
+        public static bool IsNumeric(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            return Regex.IsMatch(input, @"^[0-9]+$");
+        }
+
+        /// <summary>
+        /// 验证传入值是否为自然数（大于等于0数字，数字前面不可出现+符号）
+        /// </summary>
+        /// <param name="input">待验证值</param>
+        /// <returns>是否为自然数</returns>
+        public static bool IsNaturalNum(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            return Regex.IsMatch(input, @"^(0|[1-9][0-9]*)$");
+        }
+
+        /// <summary>
+        ///  验证传入值是否为正整数（大于0的数字，数字前面可出现+符号）
+        /// </summary> 
+        /// <param name="input">待验证值</param>
+        /// <returns>是否为正整数</returns>
+        public static bool IsPositiveInteger(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            return Regex.IsMatch(input, @"^+?[1-9][0-9]*$");
+        }
+
+        /// <summary>
+        ///  验证传入值是否为负整数（小于0的数字，数字前面可出现-符号）
+        /// </summary> 
+        /// <param name="input">待验证值</param>
+        /// <returns>是否为负整数</returns>
+        public static bool IsNegativeInteger(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            return Regex.IsMatch(input, @"^-?[1-9][0-9]*$");
+        }
+
+        /// <summary>
+        ///  验证传入值是否Int32整数
+        /// </summary>
+        /// <param name="input">待验证值</param>
+        /// <returns>是否为Int32整数</returns>
+        public static bool IsInt32(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            int result = 0;
+            return int.TryParse(input, out result);
+        }
+
+        /// <summary>
+        ///  验证传入值是否Int64长整数
+        /// </summary>
+        /// <param name="input">待验证值</param>
+        /// <returns>是否为Int64长整数</returns>
+        public static bool IsInt64(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            long result = 0;
+            return long.TryParse(input, out result);
+        }
+
+        /// <summary>
+        ///  验证传入值是否float类型值
+        /// </summary> 
+        /// <param name="input">待验证值</param>
+        /// <returns>是否为float类型值</returns>
+        public static bool IsFloat(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            float result = 0;
+            return float.TryParse(input, out result);
+        }
+
+        /// <summary>
+        ///  验证传入值是否double类型值
+        /// </summary>
+        /// <typeparam name="T">待验证值类型</typeparam>
+        /// <param name="input">待验证值</param>
+        /// <returns>是否为double类型值</returns>
+        public static bool IsDouble(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            double result = 0;
+            return double.TryParse(input, out result);
+        }
+
+        /// <summary>
+        ///  验证传入值是否decimal类型值
+        /// </summary>
+        /// <param name="input">待验证值</param>
+        /// <returns>是否为decimal类型值</returns>
+        public static bool IsDecimal(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            decimal result = 0;
+            return decimal.TryParse(input, out result);
+        }
+
+        /// <summary>
+        /// 判断是否为以分隔符整隔开的整数字符串
+        /// 分隔符必须指定，不能为空。格式：1,2,3,4,5,6
+        /// </summary>
+        /// <param name="input">待字符串集合</param>
+        /// <param name="separator">分隔符</param>
+        /// <returns>是否为以分隔符整隔开的整数字符串</returns>
+        public static bool IsNumericArray(string input, string separator)
+        {
+            if (string.IsNullOrEmpty(input)) return false;
+            if (Regex.IsMatch(input, @"^([-+]?[0-9]+([" + separator + "][-+]?[0-9]+)*)$") == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 验证是否为有效身份证号
+        /// </summary>
+        /// <param name="input">待验证身份证号码</param>
+        /// <returns>是否为有效身份证号</returns>
+        public static bool IsIDCard(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            int intLen = input.Length;
+            long n = 0;
+
+            if (intLen == 18)
+            {
+                if (long.TryParse(input.Remove(17), out n) == false || n < Math.Pow(10, 16) || long.TryParse(input.Replace('x', '0').Replace('X', '0'), out n) == false)
+                {
+                    return false;//数字验证
+                }
+                string address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";
+                if (address.IndexOf(input.Remove(2)) == -1)
+                {
+                    return false;//省份验证
+                }
+                string birth = input.Substring(6, 8).Insert(6, "-").Insert(4, "-");
+                DateTime time = new DateTime();
+                if (DateTime.TryParse(birth, out time) == false)
+                {
+                    return false;//生日验证
+                }
+                string[] arrVarifyCode = ("1,0,x,9,8,7,6,5,4,3,2").Split(',');
+                string[] Wi = ("7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2").Split(',');
+                char[] Ai = input.Remove(17).ToCharArray();
+                int sum = 0;
+                for (int i = 0; i < 17; i++)
+                {
+                    sum += int.Parse(Wi[i]) * int.Parse(Ai[i].ToString());
+                }
+                int y = -1;
+                Math.DivRem(sum, 11, out y);
+                if (arrVarifyCode[y] != input.Substring(17, 1).ToLower())
+                {
+                    return false;//校验码验证
+                }
+                return true;//符合GB11643-1999标准
+            }
+            else if (intLen == 15)
+            {
+                if (long.TryParse(input, out n) == false || n < Math.Pow(10, 14))
+                {
+                    return false;//数字验证
+                }
+                string address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";
+                if (address.IndexOf(input.Remove(2)) == -1)
+                {
+                    return false;//省份验证
+                }
+                string birth = input.Substring(6, 6).Insert(4, "-").Insert(2, "-");
+                DateTime time = new DateTime();
+                if (DateTime.TryParse(birth, out time) == false)
+                {
+                    return false;//生日验证
+                }
+                return true;//符合15位身份证标准
+            }
+            else
+            {
+                return false;//位数不对
+            }
+        }
+
+        /// <summary>
+        /// 是否为邮政编码
+        /// </summary>
+        /// <param name="input">待验证邮政编码</param>
+        /// <returns>是否为邮政编码</returns>
+        public static bool IsPostcode(string input)
+        {
+            return (IsNumeric(input) && (input.Length == 6));
+        }
+
+        /// <summary>
+        /// 是否为手机号码
+        /// </summary>
+        /// <param name="input">需验证手机号码</param>
+        /// <returns>是否为手机号码</returns>
+        public static bool IsMobile(string input)
+        {
+            return Regex.IsMatch(input, @"^1[0-9]{10}$");
+        }
+
+        /// <summary>
+        /// 验证传入IP，是否为有效IP格式
+        /// </summary>
+        /// <param name="ip">待验证IP</param>
+        /// <returns>是否为有效IP格式</returns>
+        public static bool IsIP(string ip)
+        {
+            if (string.IsNullOrEmpty(ip))
+            {
+                return false;
+            }
+
+            return Regex.IsMatch(ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$");
+        }
+        /// <summary>
+        /// 是否为URL网址
+        /// </summary>
+        /// <param name="input">需验证字符串</param>
+        /// <returns>验证结果</returns>
+        public static bool IsURL(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            return Regex.IsMatch(input, @"^(http|https)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{1,10}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&%\$#\=~_\-]+))*$");
+        }
+        /// <summary>
+        /// 验证字符串是否日期[2004-2-29|||2004-02-29 10:29:39 pm|||2004/12/31]
+        /// </summary>
+        /// <param name="input">要验证的字符串</param>
+        /// <returns></returns>
+        public static bool IsDate(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            DateTime result;
+            return DateTime.TryParse(input, out result);
+        }
+
+        /// <summary>
+        /// 是否为时间
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsTime(string timeval)
+        {
+            return Regex.IsMatch(timeval, @"^((([0-1]?[0-9])|(2[0-3])):([0-5]?[0-9])(:[0-5]?[0-9])?)$");
+        }
+        /// <summary>
+        /// 检查内存数据库不为null，并且数据库内表个数大于0
+        /// </summary>
+        /// <param name="ds">内存数据库</param>
+        /// <returns></returns>
+        public static bool IsFillDataSetTable(DataSet ds)
+        {
+            return ds != null && ds.Tables != null && ds.Tables.Count > 0;
+        }
+
+        /// <summary>
+        /// 检查表数据不为null，并且表数据行数个数大于0
+        /// </summary>
+        /// <param name="dt">表数据</param>
+        /// <returns></returns>
+        public static bool IsFillDataTableRow(DataTable dt)
+        {
+            return dt != null && dt.Rows.Count > 0;
+        }
+    }
+}
---
+++ DateTimeHelper.cs
@@ -0,0 +1,273 @@
+﻿using System;
+
+namespace CommonLibrary
+{
+    /// <summary>
+    /// 常用时间操作类
+    /// </summary>
+    public class DateTimeHelper
+    {
+        /// <summary>
+        /// 将Unix时间戳转换为DateTime类型时间
+        /// </summary>
+        /// <param name="ts"></param>
+        /// <returns></returns>
+        public static DateTime ConvertTimeByUnix(object timeStamp)
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            long time = long.Parse(string.Concat(timeStamp, "0000000"));
+            TimeSpan timeSpan = new TimeSpan(time);
+            return startTime.Add(timeSpan);
+        }
+        /// <summary>
+        /// 将时间转为时间戳
+        /// </summary>
+        /// <param name="dateTime">需要转换的时间</param>
+        /// <returns></returns>
+        public static int ConvertUnixByTime(DateTime dateTime)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            return (int)(dateTime - startTime).TotalSeconds;
+        }
+
+        /// <summary>
+        /// 将Unix时间戳转换为DateTime类型时间
+        /// </summary>
+        /// <param name="unixTime">Unix时间戳（秒）</param>
+        /// <returns></returns>
+        public static DateTime UnixTimeToDateTime(object unixTime)
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            long time = long.Parse(string.Concat(unixTime, "0000000"));
+            TimeSpan timeSpan = new TimeSpan(time);
+            return startTime.Add(timeSpan);
+        }
+
+        /// <summary>
+        /// 将DateTime时间格式转换为Unix时间戳格式
+        /// </summary>
+        /// <param name="dateTime">时间</param>
+        /// <returns> Unix时间戳</returns>
+        public static int DateTimeToUnixTime(DateTime dateTime)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            return (int)(dateTime - startTime).TotalSeconds;
+        }
+
+        /// <summary>
+        /// 将日期转换为数字
+        /// </summary>
+        /// <param name="date">日期</param>
+        /// <returns>数字</returns>
+        public static int DateToInt(DateTime date)
+        {
+            string strDate = date.ToString("yyyyMMdd");
+            return int.Parse(strDate);
+        }
+
+        /// <summary>
+        /// 将日期字符串转换为数字
+        /// </summary>
+        /// <param name="strDate">日期字符串</param>
+        /// <returns>数字</returns>
+        public static int DateToInt(string strDate)
+        {
+            if (BasicValidatorHelper.IsDate(strDate) == false) return 0;
+            DateTime date = Convert.ToDateTime(strDate);
+            strDate = date.ToString("yyyyMMdd");
+            return int.Parse(strDate);
+        }
+
+        /// <summary>
+        /// 将时间字符串转换为数字
+        /// </summary>
+        /// <param name="strDate">日期字符串</param>
+        /// <returns>数字</returns>
+        public static int TimeToInt(DateTime time)
+        {
+            string strTime = time.ToString("HHmmss");
+            return int.Parse(strTime);
+        }
+
+        /// <summary>
+        /// 将数字转换为日期字符串
+        /// </summary>
+        /// <param name="intDate">数字</param>
+        /// <returns>日期字符串</returns>
+        public static string IntToStr(int intDate)
+        {
+            string strDate = intDate.ToString("####-##-##");
+            return strDate;
+        }
+
+        /// <summary>
+        /// 将数字转换为日期
+        /// </summary>
+        /// <param name="intDate">数字</param>
+        /// <returns>日期</returns>
+        public static DateTime IntToDate(int intDate)
+        {
+            string strDate = intDate.ToString("####-##-##");
+            DateTime date = Convert.ToDateTime(strDate);
+            return date;
+        }
+
+        /// <summary>
+        /// 将数字转换为时间
+        /// </summary>
+        /// <param name="intDate">数字</param>
+        /// <returns>时间</returns>
+        public static DateTime IntToTime(int intTime)
+        {
+            string strDate = intTime.ToString().PadLeft(6, '0');
+            DateTime date = Convert.ToDateTime(strDate.Substring(0, 2) + ":" + strDate.Substring(2, 2) + ":" + strDate.Substring(4, 2));
+            return date;
+        }
+
+        /// <summary>
+        /// 整型转换成时间
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static DateTime IntToDateTime(int date, int time)
+        {
+            string intTime = time.ToString().PadLeft(6, '0');
+            return Convert.ToDateTime(string.Format("{0:####-##-##} {1}:{2}:{3}", date, intTime.Substring(0, 2), intTime.Substring(2, 2), intTime.Substring(4, 2)));
+        }
+
+        /// <summary>
+        /// 当前日期加减n天后的日期
+        /// </summary>
+        /// <param name="days">天数</param>
+        /// <returns>日期</returns>
+        public static DateTime DateAfterToday(int days)
+        {
+            return DateTime.Today.AddDays(days);
+        }
+
+        /// <summary>
+        /// 给定一个日期，返回加减n天后的日期
+        /// </summary>
+        /// <param name="date">给定日期</param>
+        /// <param name="days">天数</param>
+        /// <returns>加减后的日期</returns>
+        public static DateTime DateAfterOneDate(DateTime date, int days)
+        {
+            return date.AddDays(days);
+        }
+
+        /// <summary>
+        /// 计算两个日期相隔的天数
+        /// </summary>
+        /// <param name="date1">开始日期</param>
+        /// <param name="date2">结束日期</param>
+        /// <returns>天数</returns>
+        public static int DaysBetweenTwoDate(DateTime date1, DateTime date2)
+        {
+            TimeSpan ts = date2 - date1;
+            return ts.Days;
+        }
+
+        /// <summary>
+        /// 计算两个日期相隔的天数
+        /// </summary>
+        /// <param name="strDate1">开始日期</param>
+        /// <param name="strDate2">结束日期</param>
+        /// <returns>天数</returns>
+        public static int DaysBetweenTwoDate(string strDate1, string strDate2)
+        {
+            DateTime date1 = Convert.ToDateTime(strDate1);
+            DateTime date2 = Convert.ToDateTime(strDate2);
+            TimeSpan ts = date2 - date1;
+            return ts.Days;
+        }
+
+        /// <summary>
+        /// 计算两个日期相隔的天数
+        /// </summary>
+        /// <param name="intDate1">开始日期</param>
+        /// <param name="intDate2">结束日期</param>
+        /// <returns>天数</returns>
+        public static int DaysBetweenTwoDate(int intDate1, int intDate2)
+        {
+            DateTime date1 = IntToDate(intDate1);
+            DateTime date2 = IntToDate(intDate2);
+            TimeSpan ts = date2 - date1;
+            return ts.Days;
+        }
+
+        /// <summary>
+        /// 给定一个日期,返回当月的第一天
+        /// </summary>
+        /// <param name="date">给定日期</param>
+        /// <returns>当月的第一天</returns>
+        public static int GetMonthFirstDate(DateTime date)
+        {
+            return int.Parse(Convert.ToDateTime(date.ToString("yyyy-MM-") + "01").ToString("yyyyMMdd"));
+        }
+
+        /// <summary>
+        /// 给定一个日期,返回当月的最后一天
+        /// </summary>
+        /// <param name="date">给定日期</param>
+        /// <returns>当月的最后一天</returns>
+        public static int GetMonthLastDate(DateTime date)
+        {
+            return int.Parse(Convert.ToDateTime(date.ToString("yyyy-MM-") + "01").AddMonths(1).AddDays(-1).ToString("yyyyMMdd"));
+        }
+
+        /// <summary>        
+        /// 格式化显示时间为几个月,几天前,几小时前,几分钟前,或几秒前        
+        /// </summary>        
+        /// <param name="date">要格式化显示的时间</param>        
+        /// <returns>几个月,几天前,几小时前,几分钟前,或几秒前</returns>        
+        public static string DateStringFromNow(DateTime date)
+        {
+            TimeSpan span = DateTime.Now - date;
+            if (span.TotalDays > 60) { return date.ToShortDateString(); }
+            else if (span.TotalDays > 30) { return "1个月前"; }
+            else if (span.TotalDays > 14) { return "2周前"; }
+            else if (span.TotalDays > 7) { return "1周前"; }
+            else if (span.TotalDays > 1) { return string.Format("{0}天前", (int)Math.Floor(span.TotalDays)); }
+            else if (span.TotalHours > 1) { return string.Format("{0}小时前", (int)Math.Floor(span.TotalHours)); }
+            else if (span.TotalMinutes > 1) { return string.Format("{0}分钟前", (int)Math.Floor(span.TotalMinutes)); }
+            else if (span.TotalSeconds >= 1) { return string.Format("{0}秒前", (int)Math.Floor(span.TotalSeconds)); }
+            else { return "1秒前"; }
+        }
+        /// <summary>
+        /// 得到本周第一天(以星期一为第一天)
+        /// </summary>
+        /// <param name="datetime"></param>
+        /// <returns></returns>
+        public static DateTime GetWeekFirstDayMon(DateTime datetime)
+        {
+            //星期一为第一天
+            int weeknow = Convert.ToInt32(datetime.DayOfWeek);
+
+            //因为是以星期一为第一天，所以要判断weeknow等于0时，要向前推6天。
+            weeknow = (weeknow == 0 ? (7 - 1) : (weeknow - 1));
+            int daydiff = (-1) * weeknow;
+
+            //本周第一天
+            string FirstDay = datetime.AddDays(daydiff).ToString("yyyy-MM-dd");
+            return Convert.ToDateTime(FirstDay);
+        }
+
+        /// <summary>
+        /// 得到本周最后一天(以星期六为最后一天)
+        /// </summary>
+        /// <param name="datetime"></param>
+        /// <returns></returns>
+        public static DateTime GetWeekLastDaySat(DateTime datetime)
+        {
+            //星期六为最后一天
+            int weeknow = Convert.ToInt32(datetime.DayOfWeek);
+            int daydiff = (7 - weeknow) - 1;
+
+            //本周最后一天
+            string LastDay = datetime.AddDays(daydiff).ToString("yyyy-MM-dd");
+            return Convert.ToDateTime(LastDay);
+        }
+    }
+}
