892c07875653dd49debe56c077c41f2977b4da1f
敏强 卢 <luminqiang@outlook.com>
2019-07-08 18:06:44 +0800

    初始化

---
+++ EntityHelper.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace CommonLibrary
+{
+    /// <summary>
+    /// 实体对象辅助类
+    /// </summary>
+    public class EntityHelper
+    {
+        /// <summary>
+        /// 从一个实体类中，复制数据到另外一个实体类
+        /// </summary>
+        /// <param name="destination">目标</param>
+        /// <param name="source">来源</param>
+        /// <returns>成功复制的值个数</returns>
+        public static int CopyEntity<T, S>(T destination, S source)
+        {
+            return CopyEntity(destination, source, source.GetType(), null);
+        }
+
+        /// <summary>
+        /// 从一个实体类中，复制数据到另外一个实体类
+        /// </summary>
+        /// <param name="destination">目标</param>
+        /// <param name="source">来源</param>
+        /// <param name="excludeName">排除下列名称的属性或字段不要复制</param>
+        /// <returns>成功复制的值个数</returns>
+        public static int CopyEntity<T, S>(T destination, S source, Type templateType)
+        {
+            return CopyEntity(destination, source, templateType, null);
+        }
+
+        /// <summary>
+        /// 从一个实体类中，复制数据到另外一个实体类
+        /// </summary>
+        /// <param name="destination">目标</param>
+        /// <param name="source">来源</param>
+        /// <param name="templateType">复制的属性字段模板</param>
+        /// <param name="excludeName">排除下列名称的属性或字段不要复制</param>
+        /// <returns>成功复制的值个数</returns>
+        public static int CopyEntity<T, S>(T destination, S source, Type templateType, IList<string> excludeName)
+        {
+            int count = 0;
+            count += CopyEntityFields(destination, source, templateType, excludeName);
+            count += CopyEntityProperties(destination, source, templateType, excludeName);
+
+            return count;
+        }
+
+        /// <summary>
+        /// 从一个实体类中，把实体属性字段数据到另外一个实体类
+        /// </summary>
+        /// <param name="destination">目标</param>
+        /// <param name="source">来源</param>
+        /// <returns>成功复制的值个数</returns>
+        public static int CopyEntityFields<T, S>(object destination, S source)
+        {
+            return CopyEntityFields(destination, source, source.GetType(), null);
+        }
+
+        /// <summary>
+        /// 从一个实体类中，把实体属性字段数据到另外一个实体类
+        /// </summary>
+        /// <param name="destination">目标</param>
+        /// <param name="source">来源</param>
+        /// <param name="excludeName">排除下列名称的字段不要复制</param>
+        /// <returns>成功复制的值个数</returns>
+        public static int CopyEntityFields<T, S>(object destination, S source, Type templateType)
+        {
+            return CopyEntityFields(destination, source, templateType, null);
+        }
+
+        /// <summary>
+        /// 从一个实体类中，把实体属性字段数据到另外一个实体类
+        /// </summary>
+        /// <param name="destination">目标</param>
+        /// <param name="source">来源</param>
+        /// <param name="templateType">复制的字段模板</param>
+        /// <param name="excludeName">排除下列名称的字段不要复制</param>
+        /// <returns>成功复制的值个数</returns>
+        private static int CopyEntityFields<T, S>(T destination, S source, Type templateType, IList<string> excludeName)
+        {
+            if (destination == null || source == null)
+                return 0;
+            if (excludeName == null)
+                excludeName = new List<string>();
+            //复制成功数
+            int count = 0;
+            //目标类型
+            Type desType = destination.GetType();
+            //逐一进行复制处理
+            foreach (FieldInfo mField in templateType.GetFields())
+            {
+                if (excludeName.Contains(mField.Name))
+                    continue;
+                try
+                {
+                    FieldInfo des = desType.GetField(mField.Name);
+                    if (des != null && des.FieldType == mField.FieldType)
+                    {
+                        des.SetValue(destination, mField.GetValue(source));
+                        count++;
+                    }
+                }
+                catch
+                {
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// 从一个实体类中，把实体属性复制数据到另外一个新建的实体类
+        /// </summary>
+        /// <param name="source">来源</param>
+        /// <returns>成功复制的值个数大于0返回新建的实体类，不大于0返回null</returns>
+        public static TResult CopyNewEntityProperties<S, TResult>(S source) where S : class where TResult : class, new()
+        {
+            if (source != null)
+            {
+                TResult info = new TResult();
+                return CopyEntityProperties(info, source) > 0 ? info : null;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 从一个实体类中，把实体属性复制数据到另外一个实体类
+        /// </summary>
+        /// <param name="destination">目标</param>
+        /// <param name="source">来源</param>
+        /// <returns>成功复制的值个数</returns>
+        public static int CopyEntityProperties<T, S>(T destination, S source)
+        {
+            return CopyEntityProperties(destination, source, source.GetType(), null);
+        }
+
+        /// <summary>
+        /// 从一个实体类中，把实体属性复制数据到另外一个实体类
+        /// </summary>
+        /// <param name="destination">目标</param>
+        /// <param name="source">来源</param>
+        /// <param name="excludeName">排除下列名称的属性不要复制</param>
+        /// <returns>成功复制的值个数</returns>
+        public static int CopyEntityProperties<T, S>(T destination, S source, Type templateType)
+        {
+            return CopyEntityProperties(destination, source, templateType, null);
+        }
+
+        /// <summary>
+        /// 从一个实体类中，把实体属性复制数据到另外一个实体类
+        /// </summary>
+        /// <param name="destination">目标</param>
+        /// <param name="source">来源</param>
+        /// <param name="templateType">复制的属性模板</param>
+        /// <param name="excludeName">排除下列名称的属性不要复制</param>
+        /// <returns>成功复制的值个数</returns>
+        private static int CopyEntityProperties<T, S>(T destination, S source, Type templateType, IList<string> excludeName)
+        {
+            if (destination == null || source == null)
+                return 0;
+
+            if (excludeName == null)
+                excludeName = new List<string>();
+
+            //复制成功数
+            int count = 0;
+            //目标类型
+            Type desType = destination.GetType();
+            //逐一进行复制处理
+            foreach (PropertyInfo mProperty in templateType.GetProperties())
+            {
+                if (excludeName.Contains(mProperty.Name))
+                    continue;
+                try
+                {
+                    PropertyInfo des = desType.GetProperty(mProperty.Name);
+                    if (des != null && des.PropertyType == mProperty.PropertyType &&
+                        des.CanWrite && mProperty.CanRead)
+                    {
+                        des.SetValue(destination, mProperty.GetValue(source, null), null);
+                        //des.SetValue(destination, ChangeType(mProperty.GetValue(source, null), mProperty.PropertyType), null);
+                        count++;
+                    }
+                }
+                catch
+                {
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// 类型转换
+        /// </summary>
+        /// <param name="value">源类型值</param>
+        /// <param name="type">目标类型</param>
+        /// <returns></returns>
+        public static object ChangeType(object value, Type type)
+        {
+            if (value == null && type.IsGenericType) return Activator.CreateInstance(type);
+            if (value == null) return null;
+            if (type == value.GetType()) return value;
+            if (type.IsEnum)
+            {
+                if (value is string)
+                    return Enum.Parse(type, value as string);
+                else
+                    return Enum.ToObject(type, value);
+            }
+            if (!type.IsInterface && type.IsGenericType)
+            {
+                Type innerType = type.GetGenericArguments()[0];
+                object innerValue = ChangeType(value, innerType);
+                return Activator.CreateInstance(type, new object[] { innerValue });
+            }
+            if (value is string && type == typeof(Guid)) return new Guid(value as string);
+            if (value is string && type == typeof(Version)) return new Version(value as string);
+            if (!(value is IConvertible)) return value;
+            return Convert.ChangeType(value, type);
+        }
+
+        /// <summary>
+        /// 转换类型对象
+        /// </summary>
+        /// <typeparam name="TResult">输出类型</typeparam>
+        /// <param name="inputObj">输入对象</param>
+        /// <param name="typeObj">输出类型对象</param>
+        /// <returns>转换类型后对象</returns>
+        public static TResult ConvertEntity<TResult>(object inputObj, TResult typeObj)
+        {
+            if (inputObj != null)
+            {
+                return (TResult)inputObj;
+            }
+
+            return default;
+        }
+    }
+}
---
+++ JsonHelper.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Data;
+using fastJSON;
+using Nancy.Json;
+
+namespace CommonLibrary
+{
+    /// <summary>
+    /// JSON操作类
+    /// </summary>
+    public class JsonHelper
+    {
+        /// <summary>
+        /// 快速将对象转为JSON
+        /// </summary>
+        /// <param name="obj">对象</param>
+        /// <returns></returns>
+        public static string FastToJson(object obj)
+        {
+            return JSON.ToJSON(obj, new JSONParameters { EnableAnonymousTypes = true, UseEscapedUnicode = true });
+        }
+        /// <summary>
+        /// 快速将对象转为JSON
+        /// </summary>
+        /// <param name="obj">对象</param>
+        /// <param name="prams">附加属性</param>
+        /// <returns></returns>
+        public static string FastToJson(object obj, JSONParameters prams)
+        {
+            return JSON.ToJSON(obj, prams);
+        }
+        /// <summary>
+        /// 快速将JSON转为对象
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="json">JSON字符串</param>
+        /// <returns></returns>
+        public static T FastToObject<T>(string json) where T : class
+        {
+            return JSON.ToObject<T>(json);
+        }
+        /// <summary>
+        /// 将指定的 JSON 字符串转换为 dynamic 类型的对象
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static dynamic JsonToDynamic(string json)
+        {
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            jss.RegisterConverters(new JavaScriptConverter[] { new DynamicJsonConverter() });
+            return jss.DeserializeObject(json) as dynamic;
+        }
+        /// <summary>
+        /// 将对象转换为 JSON 字符串
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string GetJson<T>(T input)
+        {
+            string rtn = string.Empty;
+            if (input == null) return rtn;
+            JavaScriptSerializer JsonSerializer = new JavaScriptSerializer();
+            JsonSerializer.RegisterConverters(new JavaScriptConverter[] { new DataTableConverter() });
+            rtn = JsonSerializer.Serialize(input);
+            rtn = Regex.Replace(rtn, @"\\/Date\((\d+)\)\\/", match =>
+            {
+                DateTime dt = new DateTime(1970, 1, 1);
+                dt = dt.AddMilliseconds(long.Parse(match.Groups[1].Value));
+                dt = dt.ToLocalTime();
+                return dt.ToString("yyyy-MM-dd HH:mm:ss");
+            });
+            if (!string.IsNullOrEmpty(rtn) && typeof(T) == typeof(System.Data.DataTable))
+            {
+                Dictionary<string, object> dictionary = ParseFromJson<Dictionary<string, object>>(rtn);
+                return GetJson<object>(dictionary["Rows"]);
+            }
+            return rtn;
+        }
+
+        /// <summary>
+        /// 将指定的 JSON 字符串转换为 T 类型的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static T ParseFromJson<T>(string input)
+        {
+            T rtn = default(T);
+            if (string.IsNullOrEmpty(input)) return rtn;
+            JavaScriptSerializer JsonSerializer = new JavaScriptSerializer();
+            JsonSerializer.RegisterConverters(new JavaScriptConverter[] { new DataTableConverter() });
+            if (!string.IsNullOrEmpty(input) && typeof(T) == typeof(System.Data.DataTable))
+            {
+                return JsonSerializer.Deserialize<T>(string.Format("{{\"Rows\":{0}}}", input));
+            }
+            else
+            {
+                rtn = JsonSerializer.Deserialize<T>(input);
+            }
+            return rtn;
+        }
+        /// <summary>
+        /// 获取JSON字符串的键值
+        /// </summary>
+        /// <param name="json">JSON字符串</param>
+        /// <param name="key">键值</param>
+        /// <returns></returns>
+        public static string GetJsonValue(string json, string key)
+        {
+            if (string.IsNullOrEmpty(json) || !json.StartsWith("{") || !json.EndsWith("}")) return string.Empty;
+            Dictionary<string, object> dictionary = JsonHelper.ParseFromJson<Dictionary<string, object>>(json);
+            if (dictionary == null || dictionary.Count == 0) return string.Empty;
+            object returnValue = null;
+            foreach (KeyValuePair<string, object> dic in dictionary)
+            {
+                if (string.IsNullOrEmpty(dic.Key)) break;
+                if (dic.Key == key)
+                {
+                    if (dic.Value.GetType() == typeof(Dictionary<string, object>))
+                    {
+                        returnValue = JsonHelper.GetJson<Dictionary<string, object>>((Dictionary<string, object>)dic.Value);
+                    }
+                    else
+                    {
+                        returnValue = dic.Value;
+                    }
+                    break;
+                }
+            }
+            if (returnValue == null)
+            {
+                return string.Empty;
+            }
+            if (returnValue.GetType() == typeof(ArrayList))
+            {
+                return string.Empty;
+            }
+            return returnValue.ToString();
+        }
+        /// <summary>
+        /// 获取JSON字符串的键值
+        /// </summary>
+        /// <param name="json">JSON字符串</param>
+        /// <param name="key">键值</param>
+        /// <returns></returns>
+        public static object GetJsonObjectValue(string json, string key)
+        {
+            if (string.IsNullOrEmpty(json) || !json.StartsWith("{") || !json.EndsWith("}")) return string.Empty;
+            Dictionary<string, object> dictionary = JsonHelper.ParseFromJson<Dictionary<string, object>>(json);
+            if (dictionary == null || dictionary.Count == 0) return string.Empty;
+            object returnValue = null;
+            foreach (KeyValuePair<string, object> dic in dictionary)
+            {
+                if (dic.Key == key)
+                {
+                    if (dic.Value.GetType() == typeof(Dictionary<string, object>))
+                    {
+                        returnValue = JsonHelper.GetJson<Dictionary<string, object>>((Dictionary<string, object>)dic.Value);
+                    }
+                    else
+                    {
+                        returnValue = dic.Value;
+                    }
+                    break;
+                }
+            }
+            if (returnValue == null)
+            {
+                return string.Empty;
+            }
+            if (returnValue.GetType() == typeof(System.Collections.ArrayList))
+            {
+                return returnValue;
+            }
+            return returnValue.ToString();
+        }
+        /// <summary>
+        /// 获取第一个KEY-VALUE的值
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static string GetJsonValue(string json)
+        {
+            if (string.IsNullOrEmpty(json) || !json.StartsWith("{") || !json.EndsWith("}")) return string.Empty;
+            Dictionary<string, object> dictionary = JsonHelper.ParseFromJson<Dictionary<string, object>>(json);
+            if (dictionary == null || dictionary.Count == 0) return string.Empty;
+            object returnValue = null;
+            foreach (KeyValuePair<string, object> dic in dictionary)
+            {
+                if (string.IsNullOrEmpty(dic.Key)) break;
+                if (dic.Value.GetType() == typeof(Dictionary<string, object>))
+                {
+                    returnValue = JsonHelper.GetJson<Dictionary<string, object>>((Dictionary<string, object>)dic.Value);
+                }
+                else
+                {
+                    returnValue = dic.Value;
+                }
+                break;
+            }
+            if (returnValue == null)
+            {
+                return string.Empty;
+            }
+            if (returnValue.GetType() == typeof(System.Collections.ArrayList))
+            {
+                return string.Empty;
+            }
+            return returnValue.ToString();
+        }
+    }
+
+    public class DataTableConverter : JavaScriptConverter
+    {
+        public override IDictionary<string, object> Serialize(object obj, JavaScriptSerializer serializer)
+        {
+            DataTable dt = obj as DataTable;
+            Dictionary<string, object> result = new Dictionary<string, object>();
+            List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                Dictionary<string, object> row = new Dictionary<string, object>();
+                foreach (DataColumn dc in dt.Columns)
+                {
+                    row.Add(dc.ColumnName, dr[dc.ColumnName]);
+                }
+                rows.Add(row);
+            }
+            result["Rows"] = rows;
+            return result;
+        }
+
+        public override object Deserialize(IDictionary<string, object> dictionary, Type type, JavaScriptSerializer serializer)
+        {
+            DataTable dt = new DataTable();
+            ArrayList array = dictionary["Rows"] as ArrayList;
+            for (int i = 0; i < array.Count; i++)
+            {
+                Dictionary<string, object> info = array[i] as Dictionary<string, object>;
+                if (info == null || info.Count == 0) return null;
+                if (i == 0)
+                {
+                    foreach (KeyValuePair<string, object> dic in info)
+                    {
+                        dt.Columns.Add(dic.Key, dic.Value.GetType());
+                    }
+                }
+                DataRow dr = dt.NewRow();
+                foreach (KeyValuePair<string, object> dic in info)
+                {
+                    dr[dic.Key] = dic.Value.ToString();
+                }
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+
+        /// <summary>
+        /// 获取本转换器支持的类型
+        /// </summary>
+        public override IEnumerable<Type> SupportedTypes
+        {
+            get { return new Type[] { typeof(DataTable) }; }
+        }
+    }
+
+    #region DynamicJsonConverter
+    public class DynamicJsonConverter : JavaScriptConverter
+    {
+        public override object Deserialize(IDictionary<string, object> dictionary, Type type, JavaScriptSerializer serializer)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+
+            if (type == typeof(object))
+            {
+                return new DynamicJsonObject(dictionary);
+            }
+
+            return null;
+        }
+
+        public override IDictionary<string, object> Serialize(object obj, JavaScriptSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Type> SupportedTypes
+        {
+            get { return new ReadOnlyCollection<Type>(new List<Type>(new Type[] { typeof(object) })); }
+        }
+    }
+
+    public class DynamicJsonObject : DynamicObject
+    {
+        private IDictionary<string, object> Dictionary { get; set; }
+
+        public DynamicJsonObject(IDictionary<string, object> dictionary)
+        {
+            this.Dictionary = dictionary;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = this.Dictionary[binder.Name];
+
+            if (result is IDictionary<string, object>)
+            {
+                result = new DynamicJsonObject(result as IDictionary<string, object>);
+            }
+            else if (result is ArrayList && (result as ArrayList) is IDictionary<string, object>)
+            {
+                result = new List<DynamicJsonObject>((result as ArrayList).ToArray().Select(x => new DynamicJsonObject(x as IDictionary<string, object>)));
+            }
+            else if (result is ArrayList)
+            {
+                result = new List<object>((result as ArrayList).ToArray());
+            }
+
+            return this.Dictionary.ContainsKey(binder.Name);
+        }
+    }
+    #endregion 
+}
